40982995cd69223bdd635f49e9eab508e09d6627
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-08-21 12:58:21 +0300

    add activeClassName in MainMenu

--- components/MainMenu/index.jsx
+++ components/MainMenu/index.jsx
@@ -14,42 +14,42 @@ class MainMenu extends React.Component {
         return (
             <div className='main-menu'>
 
-                <Link to={ prefixLink('/')} className='main-menu__link'>
+                <Link to={ prefixLink('/')} className='main-menu__link' activeClassName='is-selected' onlyActiveOnIndex>
                     Комінг сун
                     <Ink />
                 </Link>
 
-                <Link to={ prefixLink('/home')} className={`main-menu__link ${ location.pathname === prefixLink('/home') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/home')} className='main-menu__link' activeClassName='is-selected'>
                     Домашня
                     <Ink />
                 </Link>
 
-                <Link to={ prefixLink('/about')} className={`main-menu__link ${ location.pathname === prefixLink('/about') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/about')} className='main-menu__link' activeClassName='is-selected'>
                     Про нас
                     <Ink />
                 </Link> 
 
-                <Link to={ prefixLink('/catalog')} className={`main-menu__link ${ location.pathname === prefixLink('/catalog') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/catalog')} className='main-menu__link' activeClassName='is-selected'>
                     Каталог
                     <Ink />
                 </Link> 
 
-                <Link to={ prefixLink('/portfolio')} className={`main-menu__link ${ location.pathname === prefixLink('/portfolio') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/portfolio')} className='main-menu__link' activeClassName='is-selected'>
                     Портфоліо
                     <Ink />
                 </Link> 
 
-                <Link to={ prefixLink('/furniture-restoration')} className={`main-menu__link ${ location.pathname === prefixLink('/furniture-restoration') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/furniture-restoration')} className='main-menu__link' activeClassName='is-selected'>
                     Реставрація меблів
                     <Ink />
                 </Link> 
 
-                <Link to={ prefixLink('/for-partners')} className={`main-menu__link ${ location.pathname === prefixLink('/for-partners') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/for-partners')} className='main-menu__link' activeClassName='is-selected'>
                     Партнерам
                     <Ink />
                 </Link> 
 
-                <Link to={ prefixLink('/contacts')} className={`main-menu__link ${ location.pathname === prefixLink('/contacts') ? 'is-selected' : '' }`}>
+                <Link to={ prefixLink('/contacts')} className='main-menu__link' activeClassName='is-selected'>
                     Контакти
                     <Ink />
                 </Link> 
